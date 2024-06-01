e3c2a9e924e984405ae6ba162c13ca0c5b1034ca
CodeArtisanDurjoy <da-durjoy@outlook.com>
2024-06-01 11:29:09 +0600

    20

--- Read_Json_Doc.py
+++ Read_Json_Doc.py
@@ -6,12 +6,9 @@ file = 'enwiki-country.json.gz'
 def extract_uk_article(file):
     with gzip.open(file, 'rt', encoding='utf-8') as file:
         for line in file:
-            # Parse the JSON line
             article = json.loads(line)
-            # Check if the title is "United Kingdom"
             if article['title'] == 'United Kingdom':
                 return article['text']
-
 
 uk_article_body = extract_uk_article(file)
 
